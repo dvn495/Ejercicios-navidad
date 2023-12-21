077139a25c36d459bb433366913cb7f01557f77b
Nicolas Pedraza Maldonado <152308538+dvn495@users.noreply.github.com>
2023-12-21 16:26:04 -0500

    Add files via upload

--- Ejercicio2/Principiante2.py
+++ Ejercicio2/Principiante2.py
@@ -2,42 +2,73 @@ import os
 isActive = True
 estudiantes= []
 NumEstudiantes = 0
-if (NumEstudiantes <=20):
-    while (isActive):
+PesoNormal = 0
+Sobrepeso = 0
+ObesidadI= 0
+ObesisdadII =0
+ObesidadIII= 0
+while (isActive):
+    while(NumEstudiantes <= 20):
         try:
+            os.system ("cls")
             nombre = input("Ingrese su nombre: ")
             edad= int(input("Ingrese su edad: "))
             os.system("cls")
             imc = float(input(f"ingrese su peso en kg: "))/float(input(f"ingrese su altura en m: "))**2
             print(f"Su Indice de Masa Corporal(IMC) es de: {imc}")
             if (imc > 18.5) and (imc < 24.9):
-                ClasificacionIMC = "NORMAL" 
+                ClasificacionIMC = str("NORMAL") 
                 print("Su nivel de IMC esta en normal")
             elif ((imc > 25 ) and (imc < 29.9)):
-                ClasificacionIMC = "SOBREPESO"
+                ClasificacionIMC = str("SOBREPESO")
                 print("Su nivel de IMC esta en sobrepeso")
             elif ((imc > 30) and (imc < 34.9)):
-                ClasificacionIMC = "OBESIDAD I"
+                ClasificacionIMC = str("OBESIDAD I")
                 print("Su nivel de IMC esta en obesidad I")
             elif ((imc > 35) and (imc < 39.9)):
-                ClasificacionIMC = "OBESIDAD II"
+                ClasificacionIMC = str("OBESIDAD II")
                 print("Su nivel de IMC esta en obesidad II")
             elif (imc > 40):
-                ClasificacionIMC = "OBESISDAD III" 
+                ClasificacionIMC = str("OBESISDAD III" )
                 print("Su nivel de IMC esta en obesidad III")
-            os.system ("pause")
+
+            
+
             NumEstudiantes += 1
             estudiantes.append(ClasificacionIMC)
-            Seleccion = input("¿Desea registrar otra persona? S(si) o N(no)").upper()
-            print(estudiantes)   
-            if (Seleccion == "S"):
-                isActive = True
-            elif (Seleccion == "N"):
-                isActive = False
-        
+            Seleccion = input("¿Desea registrar otra persona? (S) o pasar a ver el conteo (N): ").upper()
+            if Seleccion == "N":
+               break      
+
+            
+
         except ValueError:
             print("Error en la digitacion de los datos")
-            isActive = True
+            imcActive = True
+    
+
+    for item in estudiantes:
+              if (item == "NORMAL"):
+                PesoNormal +=1
+              elif (item == "SOBREPESO"):
+                Sobrepeso +=1
+              elif (item == "OBESIDAD I"):
+                ObesidadI +=1
+              elif(item == "OBESIDAD II"):
+                ObesisdadII +=1
+              elif(item == "OBESIDAD III"):
+                ObesidadIII +=1   
+          
+    print (f"Los estudiantes con un IMC en normal: {PesoNormal}")
+    print (f"Los estudiantes con un IMC en sobrepeso: {Sobrepeso}")
+    print (f"Los estudiantes con un IMC en obesidad I {ObesidadI}")
+    print (f"Los estudiantes con un IMC en obesidad II {ObesisdadII}")
+    print (f"Los estudiantes con un IMC en obesidad III {ObesidadIII}")
+    Seleccion = input ("Desesa ingresar mas datos? S(si) o N(no)")
+    if (Seleccion == "S"):
+        isActive = True
+    elif (Seleccion == "N"):
+        isActive = False
 
      
 
